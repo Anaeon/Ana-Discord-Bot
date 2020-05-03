47c66ed6530da518da850f00a2dde5e8235af032
Anaeon <AnaeonVaria@gmail.com>
2020-05-03 10:49:34 -0700

    Update tdt.py
    Added a check to make sure _guild was assigned by the outside thread before trying to access it.

--- modules/tdt/tdt.py
+++ modules/tdt/tdt.py
@@ -26,33 +26,36 @@ async def update():
     debug.debug(debug.D_VOMIT, 'Staring TDT update coroutine...')
     while True:
         debug.debug(debug.D_VOMIT, 'TDT Updating.')
-        try:
-            _now = datetime.now()
-            _next = storage.get_server_attribute(_guild.id, 'next_pearl_point_reset_datetime')
-            debug.debug(debug.D_VERBOSE, 'Current time: {} Next reset: {}'.format(
-                _now.strftime('%H:%M:%S'), _next.strftime('%H:%M:%S')))
-            if _now > _next:
-                await send.message(_gen_channel, scoreboard.reset_points_to_give(_guild))
-        except KeyError as e:
-            debug.debug(debug.D_ERROR, e)
-            x = datetime.today()  # today
-            if datetime.now().hour < 17:
-                y = x.replace(hour=17, minute=0, second=0, microsecond=0)  # today at 5pm
-            else:
-                if x.day + 1 <= calendar.monthrange(x.year, x.month)[1]:
-                    y = x.replace(day=x.day + 1, hour=17, minute=0, second=5, microsecond=0)  # tomorrow at 5pm
+        if _guild is None:
+            pass
+        else:
+            try:
+                _now = datetime.now()
+                _next = storage.get_server_attribute(_guild.id, 'next_pearl_point_reset_datetime')
+                debug.debug(debug.D_VERBOSE, 'Current time: {} Next reset: {}'.format(
+                    _now.strftime('%H:%M:%S'), _next.strftime('%H:%M:%S')))
+                if _now > _next:
+                    await send.message(_gen_channel, scoreboard.reset_points_to_give(_guild))
+            except KeyError as e:
+                debug.debug(debug.D_ERROR, e)
+                x = datetime.today()  # today
+                if datetime.now().hour < 17:
+                    y = x.replace(hour=17, minute=0, second=0, microsecond=0)  # today at 5pm
                 else:
-                    if x.month + 1 <= 12:
-                        #  first day of next month at 5pm
-                        y = x.replace(month=x.month + 1, day=1, hour=17, minute=0, second=5, microsecond=0)
+                    if x.day + 1 <= calendar.monthrange(x.year, x.month)[1]:
+                        y = x.replace(day=x.day + 1, hour=17, minute=0, second=5, microsecond=0)  # tomorrow at 5pm
                     else:
-                        y = x.replace(year=x.year + 1, month=1, day=1, hour=17, minute=0, second=5,
-                                      microsecond=0)
-            storage.set_server_attribute(_guild.id, 'next_pearl_point_reset_datetime', y)
-        except AttributeError as e:
-            debug.debug(debug.D_ERROR, e)
-        debug.debug(debug.D_VERBOSE, 'Time to next reset: {}'.format(
-            (storage.get_server_attribute(_guild.id, 'next_pearl_point_reset_datetime') - datetime.now())))
+                        if x.month + 1 <= 12:
+                            #  first day of next month at 5pm
+                            y = x.replace(month=x.month + 1, day=1, hour=17, minute=0, second=5, microsecond=0)
+                        else:
+                            y = x.replace(year=x.year + 1, month=1, day=1, hour=17, minute=0, second=5,
+                                          microsecond=0)
+                storage.set_server_attribute(_guild.id, 'next_pearl_point_reset_datetime', y)
+            except AttributeError as e:
+                debug.debug(debug.D_ERROR, e)
+            debug.debug(debug.D_VERBOSE, 'Time to next reset: {}'.format(
+                (storage.get_server_attribute(_guild.id, 'next_pearl_point_reset_datetime') - datetime.now())))
     # if it's past that time, reset.
         await asyncio.sleep(10)
 
